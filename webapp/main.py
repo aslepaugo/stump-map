167d7c7ca610002934a9a081eed46c2d5b31f783
Aliaksei Slepau <aslepaugo@aleksc-mac.local>
2020-10-24 05:30:49 +0300

    Added geopy, search, fixed navbar

--- webapp/main.py
+++ webapp/main.py
@@ -1,24 +1,61 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 import folium
+import sys
+from geopy.geocoders import Nominatim
+from .models import Stump
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
-def index():
+def index(city="Brno", stump_type="Stump"):
+
+    stumps = Stump.query.all()
 
     map = folium.Map(
         location=[49.211169, 16.592006],
-        zoom_start=10,
-        tiles='OpenStreetMap')
+        zoom_start=13,
+        tiles='OpenStreetMap',
+        scrollWheelZoom=False)
 
     folium.Marker([49.211169, 16.590006]).add_to(map)
-    folium.Marker(
-        [49.211109, 16.592056], icon=folium.Icon(icon='cloud')
-        ).add_to(map)
+
+    for stump in stumps:
+
+        latitude = stump.lattitude
+        longitude = stump.longitude
+
+        stump_description = "Click <a href='/details/{}' target='_PARENT'>here</a> to get more details".format(stump.id)
+
+        stump_icon = folium.Icon(
+                color='darkred',
+                icon='envira',
+                angle=180,
+                prefix='fa')
+
+        folium.Marker(
+            [latitude, longitude],
+            popup=stump_description,
+            icon=stump_icon
+            ).add_to(map)
 
     return render_template('index.html', map=map._repr_html_())
+
+
+@main.route('/search', methods=['POST'])
+def search():
+
+    city = request.form.get('city')
+    stump_type = request.form.get('type')
+
+    print(city, " ", stump_type)
+
+    # geolocator = Nominatim(user_agent="stump-map")
+    # location = geolocator.geocode(request.form.get('search'))
+    # print((location.latitude, location.longitude), file=sys.stdout)
+
+    return redirect(url_for('main.index'))
 
 
 @main.route('/profile')
