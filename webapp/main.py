a15867c983b68b9aa928d9ae4c47e850b722d019
Aliaksei Slepau <aslepaugo@aleksc-mac.local>
2020-10-24 12:36:02 +0300

    refactor main page, added city filter

--- webapp/main.py
+++ webapp/main.py
@@ -1,34 +1,59 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 import folium
+from folium import plugins
 
-from webapp.models import Stump
+from webapp.models import Stump, City, Stump_type
+from webapp.config import DEFAULT_CITY, USER_AGENT
+from geopy.geocoders import Nominatim
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
-def index(city="Brno", stump_type="Stump"):
+def index():
 
-    stumps = Stump.query.all()
+    requested_city = request.args.get('city')
+    if requested_city is None:
+        requested_city = DEFAULT_CITY
+
+    stumps = Stump.query.join(City).filter(City.name == requested_city).join(Stump_type).all()
+    stumps_count = len(stumps)
+    city_list = City.query.order_by(City.name).all()
+
+    if stumps_count == 0:
+        geolocator = Nominatim(user_agent=USER_AGENT)
+        location = geolocator.geocode(requested_city)
+        if location is None:
+            flash("Unknown city {}!".format(requested_city))
+            requested_city = DEFAULT_CITY
+            location = geolocator.geocode(requested_city)
+            stumps = Stump.query.join(City).filter(City.name == requested_city).join(Stump_type).all()
+            stumps_count = len(stumps)
+
+        base_latitude = location.latitude
+        base_longitude = location.longitude
+    else:
+        base_latitude = stumps[0].latitude
+        base_longitude = stumps[0].longitude
 
     map = folium.Map(
-        location=[49.211169, 16.592006],
+        location=[base_latitude, base_longitude],
         zoom_start=13,
         tiles='OpenStreetMap',
         scrollWheelZoom=False)
 
-    folium.Marker([49.211169, 16.590006]).add_to(map)
+    marker_cluster = plugins.MarkerCluster().add_to(map)
 
     for stump in stumps:
 
-        latitude = stump.lattitude
+        latitude = stump.latitude
         longitude = stump.longitude
 
         stump_description = "Click <a href='/details/{}' target='_PARENT'>here</a> to get more details".format(stump.id)
 
         stump_icon = folium.Icon(
-                color='darkred',
+                color=stump.stump_type.color,
                 icon='envira',
                 angle=180,
                 prefix='fa')
@@ -37,9 +62,14 @@ def index(city="Brno", stump_type="Stump"):
             [latitude, longitude],
             popup=stump_description,
             icon=stump_icon
-            ).add_to(map)
+            ).add_to(marker_cluster)
 
-    return render_template('index.html', map=map._repr_html_())
+    return render_template(
+        'index.html',
+        map=map._repr_html_(),
+        city_list=city_list,
+        city=requested_city,
+        stumps_count=stumps_count)
 
 
 @main.route('/search', methods=['POST'])
