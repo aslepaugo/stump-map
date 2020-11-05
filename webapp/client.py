81eb9da6a54e72bfb632aaba5a9278dbffb8474e
Evghenii Kunitski <kunitsky@gmail.com>
2020-11-06 00:05:21 +0100

    uploaded picture filename is based on timesnapshot, picture is placed into static/uploads

--- webapp/client.py
+++ webapp/client.py
@@ -2,19 +2,22 @@ import json
 import requests
 
 stump_json = {
-    'latitude': 49.215769,
-    'longitude': 16.580964,
+    'latitude': 49.211281,
+    'longitude': 16.591147,
     'city': 'Brno',
     'stump_type': 'dead sapling',
         }
 
-filename = '/media/sf_projects/for_pirates/IMG_6883.JPG'
+apikey = {'apikey': 'The Quick Brown Fox Jumped'}
+
+filename = '/media/sf_projects/for_pirates/IMG_6914.JPG'
 
 url = "http://localhost:5000/upload_api"
 
 files = [
     ('filename', (filename, open(filename, 'rb'), 'application/octet')),
     ('data', ('data', json.dumps(stump_json), 'application/json')),
+    ('apikey', ('apikey', json.dumps(apikey), 'application/json')),
 ]
 
 r = requests.post(url, files=files)
--- webapp/upload_api.py
+++ webapp/upload_api.py
@@ -5,18 +5,22 @@ import webapp.config as config
 import json                                                     
 import os
 from . import db
+import time
 
 
 upload_api = Blueprint('upload_api', __name__)                                   
 
 
 def save_file(uploaded_file):
-    filename = secure_filename(uploaded_file.filename)
+    filename = (
+        time.strftime("%Y%m%d-%H%M%S") + '.' + \
+        secure_filename(uploaded_file.filename).rsplit('.', 1)[1]
+    )
     # These commands are commented, while problem with filename is not solved, but it works 
-    # uploaded_file.save(os.path.join(
-    #                       config.PATH_TO_UPLOAD_IMAGES,
-    #                       filename))
-    return filename
+    uploaded_file.save(os.path.join(
+                          config.PATH_TO_UPLOAD_IMAGES,
+                          filename))
+    return 'uploads/'+filename
 
 
 def process_json(data, filename):        
@@ -37,15 +41,16 @@ def process_json(data, filename):
                     city_id = city_id_q,
                     stump_type_id = stump_type_id_q,
                     )
-
+    print(filename)
     # These commands are commented, while problem with filename is not solved, but it works    
-    # db.session.add(new_stump)
-    # db.session.commit()
+    db.session.add(new_stump)
+    db.session.commit()
 
 
 @upload_api.route('/upload_api',methods=['POST'])            
 def test_api():
-    #todo first thing firs - apikey authorization                                           
+    #todo first thing first - apikey authorization         
+    print(json.load(request.files['apikey']))                                  
     filename = save_file(request.files['filename'])
     #todo put into try
     process_json(json.load(request.files['data']),filename)
